b5ce8cb53b691bcbebc3f03bab837ddcb459984b
Denis Tokarev <blok1993@yandex.ru>
2018-03-22 21:25:38 +0300

    Добавлен eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": ["eslint:recommended"],
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 8
+    }
+};
